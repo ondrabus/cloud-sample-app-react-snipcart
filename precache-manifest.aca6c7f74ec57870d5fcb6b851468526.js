8e64f29510c3c6c184c7560d8916e007aada6258
Martin Makarsky <martinm@kentico.com>
2019-02-25 16:33:51 +0100

    Updates

---
+++ precache-manifest.aca6c7f74ec57870d5fcb6b851468526.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a84a9c1102aa390f7d788a9a4f4603c",
+    "url": "/name-of-the-repository/static/media/radio-button-checked.1a84a9c1.svg"
+  },
+  {
+    "revision": "7905ed5d4ee1f39bf54a",
+    "url": "/name-of-the-repository/static/css/main.93e80ae4.chunk.css"
+  },
+  {
+    "revision": "2159bae9fcf45e975889",
+    "url": "/name-of-the-repository/static/js/runtime~main.2159bae9.js"
+  },
+  {
+    "revision": "383eb4a78f2ae3b6bdef",
+    "url": "/name-of-the-repository/static/js/2.383eb4a7.chunk.js"
+  },
+  {
+    "revision": "171ce4e4cb28e074f5c9967f7db949b3",
+    "url": "/name-of-the-repository/static/media/info.171ce4e4.svg"
+  },
+  {
+    "revision": "3203126ed8d4f93f39c79851c592cbcb",
+    "url": "/name-of-the-repository/static/media/banner-default.3203126e.jpg"
+  },
+  {
+    "revision": "6b72dc24da21ca0e6d3c4175f81c3cfb",
+    "url": "/name-of-the-repository/static/media/our-story.6b72dc24.jpg"
+  },
+  {
+    "revision": "abf2d2b04c7833a4fedc9c07bdb5a642",
+    "url": "/name-of-the-repository/static/media/checkbox-unchecked.abf2d2b0.svg"
+  },
+  {
+    "revision": "392cb2e0295e00036c634860a8f0cbad",
+    "url": "/name-of-the-repository/static/media/checkbox-checked.392cb2e0.svg"
+  },
+  {
+    "revision": "8c6533e10367d0901e362f7d295e03b9",
+    "url": "/name-of-the-repository/static/media/radio-button.8c6533e1.svg"
+  },
+  {
+    "revision": "7905ed5d4ee1f39bf54a",
+    "url": "/name-of-the-repository/static/js/main.7905ed5d.chunk.js"
+  },
+  {
+    "revision": "b2622e38f246209fb933e8b8a6ee76ed",
+    "url": "/name-of-the-repository/static/media/PTSerif-Bold.b2622e38.woff"
+  },
+  {
+    "revision": "ccdb76a033026dbdc3f08c50f4a5263f",
+    "url": "/name-of-the-repository/static/media/SourceSansPro-Light.ccdb76a0.woff"
+  },
+  {
+    "revision": "fdc0354668675e460e18d8e6b880e634",
+    "url": "/name-of-the-repository/static/media/SourceSansPro-Regular.fdc03546.woff"
+  },
+  {
+    "revision": "54f926c7e2cffb824547c75bc5ac0b5e",
+    "url": "/name-of-the-repository/static/media/SourceSansPro-Bold.54f926c7.woff"
+  },
+  {
+    "revision": "4611bc4557599035d912ff07366ad002",
+    "url": "/name-of-the-repository/static/media/Core-icons.4611bc45.eot"
+  },
+  {
+    "revision": "6823218d348f1ba4e035908b672c745a",
+    "url": "/name-of-the-repository/static/media/Core-icons.6823218d.svg"
+  },
+  {
+    "revision": "14fb01c406a39aab06275f5578c7316f",
+    "url": "/name-of-the-repository/static/media/Core-icons.14fb01c4.woff"
+  },
+  {
+    "revision": "1b9af651cbf6b3f133faf2bede0e5266",
+    "url": "/name-of-the-repository/static/media/Core-icons.1b9af651.ttf"
+  },
+  {
+    "revision": "20b24c9f1ba599fd671f2f54bb0f16b0",
+    "url": "/name-of-the-repository/index.html"
+  }
+];
