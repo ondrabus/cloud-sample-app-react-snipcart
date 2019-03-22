821d5720b6e338dadf75270012e440478ccf610a
Martin Makarsky <martinmakarsky@gmail.com>
2019-03-22 16:44:56 +0100

    Updates

--- precache-manifest.fbe594160fe09ee559d824d058984e53.js
+++ precache-manifest.fbe594160fe09ee559d824d058984e53.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react-snipcart/static/media/radio-button-checked.1a84a9c1.svg"
   },
   {
-    "revision": "1535528692c17a64aecf",
-    "url": "/cloud-sample-app-react-snipcart/static/css/main.baf7086b.chunk.css"
+    "revision": "5482fb2dbf68395aef13",
+    "url": "/cloud-sample-app-react-snipcart/static/css/main.78ffd6f4.chunk.css"
   },
   {
     "revision": "ae40487515008aefa03f",
     "url": "/cloud-sample-app-react-snipcart/static/js/runtime~main.ae404875.js"
   },
   {
-    "revision": "091066ca8425d102d75f",
-    "url": "/cloud-sample-app-react-snipcart/static/js/2.091066ca.chunk.js"
+    "revision": "3a025d0c45ce788ab855",
+    "url": "/cloud-sample-app-react-snipcart/static/js/2.3a025d0c.chunk.js"
   },
   {
     "revision": "171ce4e4cb28e074f5c9967f7db949b3",
@@ -40,12 +40,8 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react-snipcart/static/media/radio-button.8c6533e1.svg"
   },
   {
-    "revision": "1535528692c17a64aecf",
-    "url": "/cloud-sample-app-react-snipcart/static/js/main.15355286.chunk.js"
-  },
-  {
-    "revision": "b2622e38f246209fb933e8b8a6ee76ed",
-    "url": "/cloud-sample-app-react-snipcart/static/media/PTSerif-Bold.b2622e38.woff"
+    "revision": "5482fb2dbf68395aef13",
+    "url": "/cloud-sample-app-react-snipcart/static/js/main.5482fb2d.chunk.js"
   },
   {
     "revision": "ccdb76a033026dbdc3f08c50f4a5263f",
@@ -56,16 +52,20 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react-snipcart/static/media/SourceSansPro-Regular.fdc03546.woff"
   },
   {
-    "revision": "4611bc4557599035d912ff07366ad002",
-    "url": "/cloud-sample-app-react-snipcart/static/media/Core-icons.4611bc45.eot"
-  },
-  {
     "revision": "54f926c7e2cffb824547c75bc5ac0b5e",
     "url": "/cloud-sample-app-react-snipcart/static/media/SourceSansPro-Bold.54f926c7.woff"
   },
   {
+    "revision": "b2622e38f246209fb933e8b8a6ee76ed",
+    "url": "/cloud-sample-app-react-snipcart/static/media/PTSerif-Bold.b2622e38.woff"
+  },
+  {
     "revision": "14fb01c406a39aab06275f5578c7316f",
     "url": "/cloud-sample-app-react-snipcart/static/media/Core-icons.14fb01c4.woff"
+  },
+  {
+    "revision": "4611bc4557599035d912ff07366ad002",
+    "url": "/cloud-sample-app-react-snipcart/static/media/Core-icons.4611bc45.eot"
   },
   {
     "revision": "1b9af651cbf6b3f133faf2bede0e5266",
@@ -76,7 +76,7 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react-snipcart/static/media/Core-icons.6823218d.svg"
   },
   {
-    "revision": "5d519db0814200ca142e871e11ce17c4",
+    "revision": "7a3b5a07cb7992325c7248854579f67b",
     "url": "/cloud-sample-app-react-snipcart/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cloud-sample-app-react-snipcart/precache-manifest.d7e83565eef82787a7666c055b949e53.js"
+  "/cloud-sample-app-react-snipcart/precache-manifest.fbe594160fe09ee559d824d058984e53.js"
 );
 
 workbox.clientsClaim();
